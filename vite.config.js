3187be70e2b6f6ffc22dceb86762a11ac5678156
subhendukarma <subhendu.karmakar@bipros.com>
2025-03-20 11:46:03 +0530

    Test 1234

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // assetsInclude: ['**/*.glb', '**/*.gltf'], // This ensures Vite handles your 3D model files
+  assetsInclude: ['**/*.glb', '**/*.gltf'], // This ensures Vite handles your 3D model files
   // base: '/konark-new/',
 })
